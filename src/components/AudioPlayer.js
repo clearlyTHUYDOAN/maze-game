08552b8820843688e06ceb95611419d0e654f9f1
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2016-11-29 17:34:19 -0500

    clean up variables

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -7,8 +7,9 @@ class AudioPlayer extends Component {
     componentDidUpdate() {
     const { start, stop, winner } = this.props;
     const player = this.refs.player; // Refers to audio tag in render > return.
-    if (start === true && stop === false && winner === false) { // Component lifecycle ensures this happens after togglePlay and changeSong changes their respective states.
-      player.play(); // The line that actually completes making songs play in addition to this.togglePlay.
+
+    if (start === true && stop === false && winner === false) { 
+        player.play(); 
     } 
     else if (start === true && stop === true && winner === false) {
         player.play(); // Needed to play songs on song change.
