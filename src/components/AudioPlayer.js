6e11731acd19876ad159b0815294a026beae6471
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2016-11-29 17:31:59 -0500

    clean up variables

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 
 class AudioPlayer extends Component {
-    
+
     // DEFINE METHODS AND COMPONENT LIFECYCLES. //
 
     componentDidUpdate() {
+    const { start, stop, winner } = this.props;
     const player = this.refs.player; // Refers to audio tag in render > return.
-    if (this.props.start === true && this.props.stop === false && this.props.winner === false) { // Component lifecycle ensures this happens after togglePlay and changeSong changes their respective states.
+    if (start === true && stop === false && winner === false) { // Component lifecycle ensures this happens after togglePlay and changeSong changes their respective states.
       player.play(); // The line that actually completes making songs play in addition to this.togglePlay.
     } 
-    else if (this.props.start === true && this.props.stop === true && this.props.winner === false) {
+    else if (start === true && stop === true && winner === false) {
         player.play(); // Needed to play songs on song change.
     } 
-    else if (this.props.start === true && this.props.stop === false && this.props.winner === true) {
+    else if (start === true && stop === false && winner === true) {
         player.play(); // Needed to play songs on song change.
     }
   };
