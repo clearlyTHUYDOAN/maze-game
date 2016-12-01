6cbd035c637593edbc0ebefb5082be33ed67fc56
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2016-12-01 10:41:56 -0500

    prevent scores achieved by cheating

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ class App extends Component {
       console.log(error);
     })
     } else {
-      prompt("Either you need to submit a player name in order to be added to the leaderboard OR you cheated.");
+      alert("Either you need to submit a player name in order to be added to the leaderboard OR you cheated.");
       console.log("Either you need to submit a player name in order to be added to the leaderboard OR you cheated.");
     }
   };
