a1f9f7eb590e1e06f85e81e76747a8014fff2844
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2016-12-01 11:29:13 -0500

    rules specified to prevent music repeating after quitting

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -5,16 +5,16 @@ class AudioPlayer extends Component {
     // DEFINE METHODS AND COMPONENT LIFECYCLES. //
 
     componentDidUpdate() {
-    const { start, stop, winner } = this.props;
+    const { start, stop, winner, quit, leaderboard } = this.props; // Provide specificity to prevent song playing when not needed.
     const player = this.refs.player; // Refers to audio tag in render > return.
 
-    if (start === true && stop === false && winner === false) {  // During a game.
+    if (start === true && stop === false && winner === false && quit === false && leaderboard === false) {  // During a game.
         player.play(); 
     } 
-    else if (start === true && stop === true && winner === false) { // Loss.
+    else if (start === true && stop === true && winner === false && quit === false && leaderboard === false) { // Loss.
         player.play(); // Needed to play songs on song change.
     } 
-    else if (start === true && stop === false && winner === true) { // Win.
+    else if (start === true && stop === false && winner === true && quit === false && leaderboard === false) { // Win.
         player.play(); // Needed to play songs on song change.
     }
   };
